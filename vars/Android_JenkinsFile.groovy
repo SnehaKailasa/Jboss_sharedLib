073fb3aa1e0727f88be4b50f43e9b748e9476369
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-23 15:24:10 +0530

    Update Android_JenkinsFile.groovy

--- vars/Android_JenkinsFile.groovy
+++ vars/Android_JenkinsFile.groovy
@@ -1,26 +1,38 @@
 def call(Map pipelineParams)
 {
-  node {
-    def scmVars
-    stage ('Git Checkout') {
-      scmVars = checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/Testing']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch', localBranch: "**"]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/SnehaKailasa23/Dosakaya1.git']]]
-    }
+	node {
+		def scmVars
+		def server = Artifactory.server 'server1'
+		def rtGradle = Artifactory.newGradleBuild()
+	   
+		stage ('Git Checkout') {
+		  scmVars = checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch', localBranch: "**"]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/SnehaKailasa23/Dosakaya1.git']]]
+		}
 
-    stage ('Gradle Build') {
-      if (scmVars.GIT_BRANCH.contains('master'))
-      {
-        sh './gradlew clean build assembleRelease'
-      }
-      else
-      {
-        sh './gradlew clean build assembleDebug'
-      }
-      sh ''' echo "These are the apk's generated with this build."
-      ls -l /var/lib/jenkins/workspace/Android_Project/app/build/outputs/apk '''
-    }
+		stage ('Gradle Build') {
+			rtGradle.deployer server: server, repo: 'Gradle_Test'
+			rtGradle.useWrapper = true
+			if (scmVars.GIT_BRANCH.contains('master'))
+			{
+				def buildInfo = rtGradle.run rootDir: "", buildFile: 'build.gradle', tasks: 'clean build assembleRelease'
+			}
+			else
+			{
+				def buildInfo = rtGradle.run rootDir: "", buildFile: 'build.gradle', tasks: 'clean build assembleDebug'	
+			}
+			def uploadSpec = """{
+				"files": [
+				{
+					"pattern": "app/build/outputs/apk/*release*.apk",
+					"target": "Gradle_Test/"
+				}
+				]
+			}"""
+			server.upload(uploadSpec)
+			//rtGradle.deployer.deployArtifacts buildInfo		//deploys both the artifacts
+			rtGradle.deployer.artifactDeploymentPatterns.addInclude("*release*.apk").addExclude("*debug.apk")	//deploys only release.apk
+			server.publishBuildInfo buildInfo	  
+		}
 
-    stage ('Archiving Artifacts') {
-        archiveArtifacts artifacts: 'app/build/outputs/apk/*.apk', fingerprint: true 
-    }
-  }
+	}
 }
