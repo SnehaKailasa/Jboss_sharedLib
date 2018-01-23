df09393f64fadbfada81a3f91866c58cde0cf6fd
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-23 15:23:06 +0530

    Update Jboss_JenkinsFile2.groovy

--- vars/Jboss_JenkinsFile2.groovy
+++ vars/Jboss_JenkinsFile2.groovy
@@ -63,11 +63,11 @@ node {
 					${SCRIPT, template="email_template_success.groovy"}''', subject: '$DEFAULT_SUBJECT', to: "${pipelineParams.success_recipients}") 
 			}
       
-			//sh './clean_up.sh'
+			sh './clean_up.sh'
 		}
 		catch(Exception e)
 		{
-			//sh './clean_up.sh'
+			sh './clean_up.sh'
 			properties([[$class: 'EnvInjectJobProperty', info: [loadFilesFromMaster: false, propertiesContent: "JobWorkSpace=${WORKSPACE}"], keepBuildVariables: true, keepJenkinsSystemVariables: true, on: true]])
 				emailext (
 					attachLog: true, attachmentsPattern: '*.html, output.xml', body: '''
