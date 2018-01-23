717b3ead857d3dcdeac0becd6bce93a68c42fd62
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-23 13:23:40 +0530

    Update Jboss_JF_CD.groovy

--- vars/Jboss_JF_CD.groovy
+++ vars/Jboss_JF_CD.groovy
@@ -2,10 +2,23 @@ def call(Map pipelineParams) {
 def rtMaven = Artifactory.newMavenBuild()
 node {
    try {
-      stage('Deployments') {
-				sh """ chmod 777 remote_script.sh 
-				ssh -T "${pipelineParams.remote_user}"@"${pipelineParams.remote_ip}" "bash -s" < ./remote_script.sh """
-			}
+	   stage('')
+	   {
+		def downloadSpec = """{
+         	   "files": [
+          	    {
+              		"pattern": " Gradle_Test/Android/GGK-21/unspecified/*.apk",
+              		"target": "artifactories/"
+           	    }
+         	]
+        	}"""
+        	server.download(downloadSpec)
+	   }
+	   stage('Deployments') 
+	   {
+		sh """ chmod 777 remote_script.sh 
+		ssh -T "${pipelineParams.remote_user}"@"${pipelineParams.remote_ip}" "bash -s" < ./remote_script.sh """
+	   }
    }
    catch()
    {
