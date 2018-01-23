69506926b48645f3262accf2f6518bdd1c12e914
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-23 17:03:42 +0530

    Update Jboss_JenkinsFile2.groovy

--- vars/Jboss_JenkinsFile2.groovy
+++ vars/Jboss_JenkinsFile2.groovy
@@ -50,7 +50,6 @@ node {
 				Reason = "Artifacts Deployment Failed"
 				rtMaven.deployer.deployArtifacts buildInfo
 			  	server.publishBuildInfo buildInfo
-				sh 'sleep 100s'
 			}
 			
 			/*stage('Deployments') {
