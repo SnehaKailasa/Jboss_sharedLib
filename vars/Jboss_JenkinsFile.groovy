c2e3123d42a0f03ff79dabe7bee319565ac84275
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-23 10:10:01 +0530

    Create Jboss_JenkinsFile.groovy

---
+++ vars/Jboss_JenkinsFile.groovy
@@ -0,0 +1,71 @@
+def call(Map pipelineParams) {
+def rtMaven = Artifactory.newMavenBuild()
+node {
+		try {
+			cleanWs()
+			stage ('Source Code Checkout') {
+				Reason = "GIT Checkout Failed"
+				checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/SnehaKailasa23/Java_sample_app.git']]]
+				//checkout scm
+			}
+			
+			stage('Maven Build') {
+				Reason = "Maven Build Failed"
+				rtMaven.tool = 'maven'							//Defining maven tool //
+				// Maven build starts here //
+				def mvn_version = tool 'maven'
+				withEnv( ["PATH+MAVEN=${mvn_version}/bin"]  ) {
+					buildInfo = rtMaven.run pom: 'SpringMVCSecurityXML/pom.xml', goals: 'clean install -Dmaven.test.skip=true' 
+				}
+			}
+			
+			stage('Docker-Compose and RFW'){
+        Reason = "Docker Compose Or RFW Failed"
+				sh """ sudo docker-compose up -d
+				sudo chmod 777 clean_up.sh
+				sudo chmod 777 wait_for_robot.sh 
+				./wait_for_robot.sh """
+				step([$class: 'RobotPublisher',
+					outputPath: "/home/robot/results",
+					passThreshold: 0,
+					unstableThreshold: 0,
+					otherFiles: ""])
+				// If Robot Framework test case fails, then the build will be failed //	
+				if("${currentBuild.result}" == "FAILURE"){	
+					sh ''' ./clean_up.sh
+					exit 1'''
+				}
+			}
+      
+      stage ('Pushing Artifacts'){		
+				Reason = "Artifacts Deployment Failed"
+				rtMaven.deployer.deployArtifacts buildInfo
+			  server.publishBuildInfo buildInfo
+			}
+			
+			stage('Deployments') {
+				sh """ chmod 777 remote_script.sh 
+				ssh -T "${pipelineParams.remote_user}"@"${pipelineParams.remote_ip}" "bash -s" < ./remote_script.sh """
+			}
+			
+			stage('Triggering QA Job') {
+				build job: 'Docker_registry', wait: false
+			}
+      
+      stage ('Email Notifications') {
+				properties([[$class: 'EnvInjectJobProperty', info: [loadFilesFromMaster: false, propertiesContent: "JobWorkSpace=${WORKSPACE}"], keepBuildVariables: true, keepJenkinsSystemVariables: true, on: true]])
+				emailext (
+					attachLog: true, attachmentsPattern: '*.html, output.xml', body: '''
+					${SCRIPT, template="email_template_success.groovy"}''', subject: '$DEFAULT_SUBJECT', to: "${pipelineParams.success_recipients}") 
+			}
+      
+			sh './clean_up.sh'
+		}
+		catch(Exception e)
+		{
+			sh './clean_up.sh'
+			println "In catch block"
+			sh 'exit 1'
+		}
+	}
+}
