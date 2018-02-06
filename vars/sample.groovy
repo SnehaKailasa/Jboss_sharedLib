d767b65f4c62d45e310e7a933c66be71e4a1990d
SnehaKailasa <sneha.kailasa@ggktech.com>
2018-02-06 18:59:13 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,7 +1,13 @@
-def call( def temp)
+def call( Map temp)
 {
   println "Value of a is " +temp.a
   println temp.a
   println temp.b
   return temp.a
+}
+def call(){
+  println "Call()"
+}
+def call(def a){
+  println "hi "+a
 }
